053add1056dc83e1133a7b67926d55628932b871
Unknown <l.a.g.o.0897@gmail.com>
2018-11-12 20:50:05 -0500

    Jaja
    Se agrego el jaja

--- views/site/index.php
+++ views/site/index.php
@@ -9,7 +9,7 @@ $this->title = 'My Yii Application';
     <div class="jumbotron">
         <h1>Ya nos quedo la plantilla Hola</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">You have successfully created your jajajajajja.</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
